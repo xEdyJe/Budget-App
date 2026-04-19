c0de2f7795e468a0df781e7206324aebc966be25
Edy <burdaedy@gmail.com>
2026-04-19 03:56:43 +0300

    test

--- app/api/banking/connect/route.ts
+++ app/api/banking/connect/route.ts
@@ -14,7 +14,7 @@ export async function GET(req: NextRequest) {
     const jwt = await new jose.SignJWT({})
       .setProtectedHeader({ alg: 'RS256', kid: appId })
       .setIssuer(appId)
-      .setAudience(['enablebanking.com'])
+      .setAudience('https://enablebanking.com')
       .setIssuedAt(now)
       .setExpirationTime(now + 3600)
       .sign(privateKey);
